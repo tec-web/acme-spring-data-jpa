3773999224fa204eebd27eea79018f5923f2cc38
Tec Web <jamatitla@gmail.com>
2021-02-10 11:46:57 -0600

    Initial commit

---
+++ src/main/java/com/tecweb/spring/jpa/AcmeSpringDataJpaApplication.java
@@ -0,0 +1,13 @@
+package com.tecweb.spring.jpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcmeSpringDataJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcmeSpringDataJpaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tecweb/spring/jpa/AcmeSpringDataJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tecweb.spring.jpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcmeSpringDataJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
